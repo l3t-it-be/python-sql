cef221ba097c5e5727a215709565c16260ec5bcb
KateBelova <ka7e.online@gmail.com>
2024-11-29 18:06:11 +0300

    validation fixed

--- mobile_app/validations.py
+++ mobile_app/validations.py
@@ -5,10 +5,10 @@ class Validation:
     @staticmethod
     def get_valid_months_input() -> int:
         while True:
-            try:
-                months = int(input(config.INPUT_MONTHS_QUANTITY))
-                return months
-            except ValueError:
+            months_input = input(config.INPUT_MONTHS_QUANTITY)
+            if months_input.isdigit() and int(months_input) > 0:
+                return int(months_input)
+            else:
                 print(config.INCORRECT_VALUE)
                 while True:
                     choice = input(config.WISH_TO_CONTINUE).strip().lower()
